e10e76d412a18fb42c63e2eb1dd1012941f8718c
fatkulnurk <fatkulnurk@gmail.com>
2024-04-27 23:01:38 +0700

    [posts] implement service & controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,20 +1,85 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Services\PostServices\PostServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
+    public function __construct(
+        protected PostServiceInterface $postService,
+    )
+    {
+    }
+
     public function discoverPost(Request $request)
     {
+        try {
+            $posts = $this->postService->getPosts();
+        } catch (\Exception $exception) {
+            return response()->json([
+                'ok' => false,
+                'err' => 'ERR_BAD_REQUEST',
+                'msg' => $exception->getMessage(),
+            ], 400);
+        }
+
+        return response()->json([
+            'ok' => true,
+            'data' => [
+                'posts' => $posts,
+            ],
+        ]);
     }
 
     public function requestPhotoUrl(Request $request)
     {
+        try {
+            $data = $this->postService->generatePhotoUrl();
+        } catch (\Exception $exception) {
+            return response()->json([
+                'ok' => false,
+                'err' => 'ERR_BAD_REQUEST',
+                'msg' => $exception->getMessage(),
+            ], 400);
+        }
+
+        return response()->json([
+            'ok' => true,
+            'data' => $data,
+        ]);
     }
 
     public function storePost(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'photo_url' => ['required', 'string', 'max:60000'],
+            'caption' => ['required', 'string', 'max:60000'],
+        ]);
+
+        try {
+            if ($validator->fails()) {
+                throw new \Exception($validator->errors()->first());
+            }
+
+            $data = $this->postService->store(
+                $request->user(),
+                $validator->valid()
+            );
+        } catch (\Exception $exception) {
+            return response()->json([
+                'ok' => false,
+                'err' => 'ERR_BAD_REQUEST',
+                'msg' => $exception->getMessage(),
+            ], 400);
+        }
+
+        return response()->json([
+            'ok' => true,
+            'data' => $data,
+        ]);
     }
 }
