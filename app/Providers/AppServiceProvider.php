1c998394eeb0951f688c28c7f02f7b63ef4bbebe
fatkulnurk <fatkulnurk@gmail.com>
2024-04-27 23:16:55 +0700

    [posts] bind to di

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Posts\Eloquent\PostRepository;
+use App\Repositories\Posts\PostRepositoryInterface;
+use App\Services\PostServices\PostService;
+use App\Services\PostServices\PostServiceInterface;
 use App\Services\UserServices\UserService;
 use App\Services\UserServices\UserServiceInterface;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->app->bind(PostRepositoryInterface::class, PostRepository::class);
+        $this->app->bind(PostServiceInterface::class, PostService::class);
         $this->app->singleton(UserServiceInterface::class, UserService::class);
     }
 }
