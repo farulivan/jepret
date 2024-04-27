7b2e787301b8edadd35b69051e90a1783a1884fd
fatkulnurk <fatkulnurk@gmail.com>
2024-04-27 22:53:31 +0700

    [posts] add method store

--- app/Services/PostServices/PostServiceInterface.php
+++ app/Services/PostServices/PostServiceInterface.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\PostServices;
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Collection;
 
 interface PostServiceInterface
@@ -9,4 +11,6 @@ interface PostServiceInterface
     public function getPosts(): Collection|array|null;
 
     public function generatePhotoUrl(): string|null;
+
+    public function store(Authenticatable|User $user, array $data);
 }
