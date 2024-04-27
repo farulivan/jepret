07770bc80653a86b9484119eb824c39c2140ce47
farulivan <farulivan@gmail.com>
2024-04-27 22:57:43 +0700

    fix: user repository construct

--- app/Repositories/User/Eloquent/UserRepository.php
+++ app/Repositories/User/Eloquent/UserRepository.php
@@ -11,7 +11,7 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
 
     public function __construct(User $model)
     {
-        parent::__construct($model);
+        $this->model = $model;
     }
 
     /**
