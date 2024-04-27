8ed3ec4fa3de1afdd1b75d105ea0e9d36849c5e3
farulivan <farulivan@gmail.com>
2024-04-27 16:08:04 +0700

    feat: change the migrations, please do fresh migrate

--- database/migrations/2024_04_27_030113_create_users_table.php
+++ database/migrations/2024_04_27_030113_create_users_table.php
@@ -9,23 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email', 64)->unique();
+            $table->string('handle', 64)->unique();
+            $table->text('password'); // Note: Using text for simplification as per spec
+            $table->timestamps(); // Adds created_at and updated_at columns
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
