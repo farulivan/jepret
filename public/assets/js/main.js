a076cc51279304f5a6d9dd945899afaf4dc074c4
Robbyn Rahmandaru <robzlabz@gmail.com>
2024-04-28 10:18:17 +0700

    initial blade template

---
+++ public/assets/js/main.js
@@ -0,0 +1,54 @@
+function selectImage(event) {
+    event.preventDefault();
+    const imageFileInput = document.getElementById("imageFileInput");
+    imageFileInput.click();
+}
+
+function openModalCreatePost() {
+    // check whether FileReader is supported on client browser
+    if (!FileReader) {
+        alert("File Reader is not supported!")
+        return
+    }
+    
+    // set file content to preview container
+    const imageFileInput = document.getElementById("imageFileInput");
+    const fr = new FileReader();
+    fr.onload = function() {
+        document.getElementById("selectedImage").src = fr.result;
+    }
+    fr.readAsDataURL(imageFileInput.files[0]);
+    
+    // display modal
+    const modalCreatePost = document.getElementById("modalCreatePost");
+    modalCreatePost.setAttribute("open", true);
+
+    // set focus on image caption
+    document.getElementById('imageCaption').focus();
+}
+
+function closeModalCreatePost(event) {
+    event.preventDefault();
+
+    // hide modal
+    const modalCreatePost = document.getElementById("modalCreatePost");
+    modalCreatePost.removeAttribute("open");
+
+    // reset form values
+    const formCreatePost = document.getElementById("formCreatePost");
+    formCreatePost.reset();
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const imageFileInput = document.getElementById("imageFileInput");
+    imageFileInput.onchange = openModalCreatePost;
+
+    const formCreatePost = document.getElementById("formCreatePost");
+    formCreatePost.onsubmit = closeModalCreatePost;
+
+    const btnCancelCreatePost = document.getElementById("btnCancelCreatePost");
+    btnCancelCreatePost.onclick = closeModalCreatePost;
+
+    const btnSelectImage = document.getElementById("btnSelectImage");
+    btnSelectImage.onclick = selectImage;
+});
