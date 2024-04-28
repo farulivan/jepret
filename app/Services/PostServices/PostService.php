02716a2101b6ba539ce2128fb59f17bc866bdb16
farulivan <farulivan@gmail.com>
2024-04-29 00:21:57 +0700

    try to fix in the last time but still failed

--- app/Services/PostServices/PostService.php
+++ app/Services/PostServices/PostService.php
@@ -26,6 +26,8 @@ class PostService implements PostServiceInterface
         $s3Client = new S3Client([
             'version' => 'latest',
             'region' => config('filesystems.disks.s3.region'),
+            'use_path_style_endpoint' => config('filesystems.disks.s3.use_path_style_endpoint'),
+            'endpoint' => config('filesystems.disks.s3.endpoint'),
             'credentials' => [
                 'key' => config('filesystems.disks.s3.key'),
                 'secret' => config('filesystems.disks.s3.secret'),
